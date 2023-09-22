e4a233ce890e4b277c859795a172b43c12680c0b
SU-T <guitaristpeet@gmail.com>
2023-09-22 15:01:12 +0700

    iniitally constructed Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,37 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <nav>
+      <div className="logo-container">
+        <Link className="logo" to="/">
+          poveda
+        </Link>
+      </div>
+      <div className="navlinks-container">
+        <div className="navlinks">
+          <Link className="link" to="/">
+            about us
+          </Link>
+          <div className="visit-container">
+            <Link className="link" to="/">
+              visit
+            </Link>
+            <div className="visit-icon"></div>
+          </div>
+          <Link className="link" to="/">
+            pricing
+          </Link>
+          <Link className="link" to="/">
+            contact
+          </Link>
+        </div>
+        <div className="hamburger-menu-container"></div>
+      </div>
+    </nav>
+  );
 };
 export default Navbar;
+
+const Wrapper = styled.nav``;
