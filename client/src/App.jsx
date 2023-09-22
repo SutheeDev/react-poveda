5103b938ca15e74dc12352dc14e715f1e3f1a7c5
SU-T <guitaristpeet@gmail.com>
2023-09-22 13:59:49 +0700

    constructed initial routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,40 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <SharedSiteLayout />,
+    children: [
+      {
+        path: "Landing",
+        element: <Landing />,
+      },
+      {
+        path: "AboutUs",
+        element: <AboutUs />,
+      },
+      {
+        path: "Visit",
+        element: <Visit />,
+      },
+      {
+        path: "Adventure",
+        element: <Adventure />,
+      },
+      {
+        path: "Culture",
+        element: <Culture />,
+      },
+      {
+        path: "Relax",
+        element: <Relax />,
+      },
+      {
+        path: "Pricing",
+        element: <Pricing />,
+      },
+      {
+        path: "Contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
