e4a78549355cf9004777b5ee902fd758302fbe6d
SU-T <guitaristpeet@gmail.com>
2023-09-26 20:33:21 +0900

    added hambergur-menu, close, and arrow-down icons to Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { BiMenu } from "react-icons/bi";
+import { BiX } from "react-icons/bi";
+import { BiSolidDownArrow } from "react-icons/bi";
 
 const Navbar = () => {
   return (
@@ -18,6 +21,9 @@ const Navbar = () => {
             <Link className="link" to="/">
               visit
             </Link>
+            <div className="arrow-down">
+              <BiSolidDownArrow />
+            </div>
             <div className="visit-icon"></div>
           </div>
           <Link className="link" to="/">
@@ -27,7 +33,14 @@ const Navbar = () => {
             contact
           </Link>
         </div>
-        <div className="hamburger-menu-container"></div>
+        <div className="hamburger-menu-container">
+          <div className="hamburger-open">
+            <BiMenu />
+          </div>
+          <div className="hamburger-close">
+            <BiX />
+          </div>
+        </div>
       </div>
     </nav>
   );
