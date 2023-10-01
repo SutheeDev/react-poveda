45e957ba47495a6bc84ed62814288c13ad514265
SU-T <guitaristpeet@gmail.com>
2023-10-01 15:25:29 +0700

    styled main-container and navlinks-container

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -93,8 +93,181 @@ export default Navbar;
 
 const Wrapper = styled.nav`
   background-color: var(--green-2);
+  box-shadow: var(--navbar-shadow);
+  .main-container {
+    padding: 1em 1.5em;
+
+    /* Separate Logo and everything else */
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+  .navlinks-container {
+    text-align: right;
+
+    /* Switch the order and get hamburger menu above the rest of links */
+    display: flex;
+    flex-direction: column-reverse;
+  }
 
   @media screen and (min-width: 950px) {
     background-color: transparent;
+    box-shadow: none;
+    .main-container {
+      padding: 1.7em 1.7em;
+      align-items: center;
+    }
+    .navlinks-container {
+      flex-direction: row;
+    }
   }
 `;
+
+// const Wrapper = styled.nav`
+//   background-color: var(--green-2);
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   height: 75px;
+//   box-shadow: var(--navbar-shadow);
+
+//   .navlinks-container {
+//     display: flex;
+//     justify-content: space-between;
+//     align-items: center;
+//     position: relative;
+//   }
+//   .navlinks {
+//     width: 100vw;
+//     position: absolute;
+//     top: 53px;
+//     right: 0;
+//     text-align: right;
+//     display: flex;
+//     flex-direction: column;
+
+//     z-index: -1;
+//     margin-top: -190px;
+//     transition: var(--global-transition);
+//   }
+//   .navlinks.show {
+//     margin-top: 0;
+//   }
+//   .link {
+//     color: var(--white);
+//     text-transform: uppercase;
+//     padding: 0.9em 0;
+//     padding-right: var(--side-padding);
+//     background-color: var(--green-2);
+//   }
+//   .sub-link {
+//     background-color: var(--green-4);
+//   }
+//   .visit-container {
+//     background-color: var(--green-2);
+//     display: flex;
+//     flex-direction: row-reverse;
+//     align-items: center;
+//   }
+//   .arrow-down {
+//     color: var(--white);
+//     display: flex;
+//     font-size: 0.8rem;
+//     margin-right: 0.6em;
+//     cursor: pointer;
+//   }
+//   .visit-subMenu {
+//     position: relative;
+//     z-index: -2;
+//     margin-top: -142px;
+
+//     transition: var(--global-transition);
+//   }
+//   .visit-subMenu.open {
+//     margin-top: 0;
+//   }
+//   .subLink-container {
+//     width: 100vw;
+//     display: flex;
+//     flex-direction: column;
+//     background-color: var(--green-4);
+//   }
+//   .hamburger-menu-container {
+//     display: grid;
+//     place-items: center;
+//     padding-right: var(--side-padding);
+//   }
+//   .hamburger-open,
+//   .hamburger-close {
+//     color: var(--white);
+//     font-size: 2rem;
+//     grid-area: 1/1;
+//     display: flex;
+//     cursor: pointer;
+//   }
+//   .link.alter-shadow {
+//     box-shadow: var(--navbar-shadow);
+//   }
+//   @media screen and (min-width: 900px) {
+//     height: 100px;
+//     .hamburger-menu-container {
+//       display: none;
+//     }
+//     .navlinks {
+//       width: 100%;
+//       position: relative;
+//       top: 0;
+//       text-align: right;
+//       flex-direction: row;
+
+//       z-index: 1;
+//       margin-top: 0;
+//     }
+//     .visit-subMenu {
+//       position: absolute;
+//       top: 47px;
+//       left: 27%;
+//       margin-top: 0;
+//       opacity: 0;
+//       z-index: -99;
+
+//       transition: var(--global-transition);
+//     }
+//     .subLink-container {
+//       width: 100%;
+//       padding: 0.9em var(--side-padding);
+//       border-radius: var(--border-radius);
+//       background-color: var(--green-2);
+//       align-items: flex-start;
+//     }
+//     .visit-container {
+//       flex-direction: row;
+//       .link {
+//         padding-right: 0;
+//       }
+//       .arrow-down {
+//         font-size: 0.8rem;
+//         margin-left: 0.6em;
+//         margin-right: 0;
+//       }
+//     }
+//     .visit-subMenu.open {
+//       opacity: 1;
+//       z-index: 5;
+//       top: 67px;
+//     }
+//     .pricing {
+//       padding-left: var(--side-padding);
+//     }
+//     .sub-link {
+//       padding-right: 0;
+//       background-color: var(--green-2);
+//     }
+//     .link {
+//       font-size: 0.875rem;
+//     }
+//     .link.alter-shadow {
+//       box-shadow: none;
+//     }
+//   }
+// `;
