d1c4f1cb4c3643697f63eac2dbbfcf13128894cc
SU-T <guitaristpeet@gmail.com>
2023-10-07 15:48:47 +0700

    added padding-top to the Landing on the small screen

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,121 +1,17 @@
+import styled from "styled-components";
+
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <h1>Landing</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. In quasi amet
-        libero magni quibusdam est atque earum asperiores voluptatum. Repellat
-        totam necessitatibus recusandae error quam ea ratione nihil voluptates
-        vero eveniet. Amet repudiandae ipsam laboriosam aperiam incidunt
-        tempore, asperiores labore explicabo neque possimus delectus modi
-        debitis doloribus provident aliquid culpa. Porro saepe consectetur
-        aperiam voluptates. Culpa eos officia praesentium eveniet nisi sunt
-        nesciunt perferendis nemo debitis. Pariatur nobis voluptates, reiciendis
-        soluta sunt quos atque nemo laudantium tempora quasi consectetur
-        quibusdam! Ipsum, molestiae. Non mollitia quam vitae delectus iure sunt
-        ratione dignissimos adipisci maxime, libero perspiciatis doloribus quas
-        odio nostrum esse, eaque dolorum maiores vero nisi eum! Neque, debitis.
-        Facere consectetur dolores, sint iure aspernatur voluptatem ducimus
-        aliquam saepe deserunt eum totam atque beatae maiores recusandae non
-        rerum. Fugit id, laboriosam delectus, nihil itaque aut sint blanditiis
-        tempora maiores vero molestias aspernatur similique quas vel
-        perspiciatis velit. Sapiente optio porro unde harum minus molestiae
-        doloremque dolores aliquam beatae cupiditate perspiciatis pariatur, id
-        repudiandae vel voluptatem veritatis voluptas magni totam natus nobis
-        laboriosam. Blanditiis itaque minima pariatur sequi harum dignissimos,
-        sit est doloremque in totam minus quisquam hic repudiandae facere quae
-        temporibus eum nulla possimus fugit libero consectetur accusamus eaque
-        debitis quaerat? Voluptatem reprehenderit delectus quaerat reiciendis
-        suscipit odio ipsam fuga doloremque ea soluta ratione expedita
-        praesentium odit repudiandae veritatis, cupiditate non mollitia eveniet
-        animi voluptatibus ipsum dolorum officia! Id corporis aspernatur
-        voluptates ducimus numquam cumque quae doloremque minus dolore illum
-        repudiandae autem, sed totam. Alias laboriosam rerum quaerat! Quisquam
-        soluta blanditiis neque, id consectetur architecto suscipit dolores
-        tenetur illo voluptas quas. Asperiores fugit, voluptate unde ipsa quod
-        maiores magnam vel atque sunt ullam aspernatur cumque ratione eum non
-        eaque, error perferendis nemo repudiandae? Aperiam voluptatibus
-        explicabo sequi necessitatibus quo error molestiae labore omnis mollitia
-        fuga aut obcaecati amet vel, cumque quia reiciendis. Culpa doloremque
-        provident impedit hic, facere natus tempore iste temporibus, assumenda
-        ipsa nostrum magnam maxime enim id libero non aspernatur? Modi quaerat
-        repudiandae minima eaque amet dicta praesentium sit maiores suscipit est
-        officia quod, nulla impedit ut blanditiis iste aliquid aspernatur nobis
-        molestias tenetur necessitatibus aperiam tempora laudantium. Eos,
-        voluptas. A, placeat at. Fuga, et nobis veritatis ullam minima quod
-        consectetur doloremque velit iusto repudiandae id ratione animi magni
-        unde numquam obcaecati reiciendis veniam laudantium voluptas! Facere,
-        sequi tempore sunt amet ullam asperiores pariatur temporibus cupiditate
-        sit libero quae nesciunt, consequatur laboriosam facilis laborum
-        expedita modi commodi omnis quidem distinctio repellendus! Rem non quos
-        sint illo libero fugit aliquam quibusdam alias, mollitia porro
-        voluptatem. Quaerat voluptatibus suscipit explicabo maxime dolorum.
-        Explicabo optio consequatur officiis? Eos enim autem est explicabo
-        aliquid unde, harum quod sunt nobis similique, obcaecati voluptates?
-        Corporis ab consequuntur dolores ut nostrum quae unde assumenda eius,
-        perspiciatis beatae accusantium similique asperiores! Nulla facilis non
-        numquam ipsam temporibus provident dignissimos consequuntur alias
-        suscipit officia velit, commodi exercitationem voluptas iure nihil
-        repellendus? Atque, ratione dolor animi id provident dolorem saepe
-        placeat officiis? Impedit, quis at tenetur est deleniti repudiandae ab
-        soluta laudantium nulla incidunt iste dicta sunt, hic, rem architecto
-        minima tempora saepe sed quam nisi debitis? Ad laborum deserunt atque
-        hic amet rerum necessitatibus sit, doloremque quasi beatae ratione
-        voluptate expedita nulla quia nam, consectetur saepe. Distinctio, quam
-        corporis possimus deserunt reiciendis reprehenderit sint aspernatur
-        error alias. Autem ullam reprehenderit eveniet officiis atque
-        reiciendis, vitae tempore optio excepturi! Ipsam nobis cupiditate quasi
-        maiores minima deleniti distinctio qui eaque. Ab reiciendis nihil esse
-        modi eaque, culpa inventore blanditiis? Tenetur libero error sequi
-        perferendis, autem alias suscipit fuga provident dolores ad officia
-        repellat ex aperiam voluptas, pariatur commodi earum eligendi accusamus
-        esse non aliquam! Dolorem quae voluptatibus laborum fugiat accusamus
-        iure veniam, quam porro voluptatum qui, cumque modi ipsam architecto
-        impedit similique neque repellat cupiditate, veritatis ab totam aut non.
-        Aut excepturi nemo dolorem non sequi impedit officiis, eos eum.
-        Asperiores, corrupti consequuntur repellendus delectus itaque recusandae
-        ullam quod nisi? Accusamus quia pariatur praesentium quasi consequatur
-        amet laudantium quisquam expedita quibusdam dicta. Quo quas porro,
-        expedita, esse ipsum asperiores, numquam culpa a quam dicta aperiam!
-        Architecto consequatur repudiandae quod repellat vel impedit ullam,
-        tenetur quos cupiditate eaque facilis ipsam sit possimus, dolores
-        voluptatibus! Cumque blanditiis nihil temporibus nesciunt, sed illo odit
-        suscipit. Beatae illo eum necessitatibus corporis neque veniam quo sint
-        quae mollitia iure, esse voluptatibus voluptate labore placeat
-        distinctio, ab cupiditate rerum recusandae eaque. Eligendi quaerat
-        molestiae atque repellendus omnis! Cupiditate natus optio doloremque!
-        Numquam, natus. Eos totam quod aliquam architecto soluta assumenda iste
-        accusamus similique consectetur. Consequatur reprehenderit minus nam
-        commodi, saepe voluptas deleniti assumenda! Dolore quis iusto voluptatum
-        nihil, quibusdam tempore excepturi quod laboriosam! Delectus repudiandae
-        iusto, sed omnis at placeat dolore minima cum voluptatibus excepturi
-        assumenda vero. Eos quos consectetur quis maxime, aliquid ratione ut
-        officia vel odio minus nam recusandae ducimus. Repudiandae, cum. Odit a
-        voluptate corporis quibusdam consequuntur doloremque corrupti officiis
-        veniam quasi rerum quia animi laudantium cumque dolorem officia
-        excepturi quae ut, qui optio dignissimos. Esse reprehenderit obcaecati
-        rem consequatur quos, repellendus facere at nisi enim, quisquam sunt cum
-        itaque deserunt eligendi explicabo eaque aliquam, nostrum quo hic
-        beatae. Id odit exercitationem cum. Recusandae cupiditate ipsam sequi
-        architecto placeat modi voluptatibus minima totam dolorum tempora rem
-        doloribus corrupti iusto nulla a maiores ad, neque illo ex eos error
-        magnam iure, ipsum beatae. Placeat nisi animi iure qui voluptatem
-        dignissimos, voluptatum asperiores molestias sint quidem consequuntur
-        magnam similique ipsum fugiat incidunt in ab consectetur suscipit quam!
-        Incidunt veniam cupiditate temporibus quis, natus quam odio dolores
-        corrupti inventore enim, animi odit perferendis aut molestiae nihil quo,
-        sit necessitatibus quia aspernatur repellendus eveniet aliquid esse nam.
-        Nisi, iusto voluptatem at consequatur modi cumque vero! Molestiae
-        voluptatem suscipit unde, laborum magnam architecto! Consectetur sequi
-        illo molestias eligendi, veniam, amet itaque tempore eum saepe nulla
-        tempora nesciunt enim aut! Esse corporis laborum excepturi sed cumque
-        natus aliquam voluptatem, odio assumenda aut libero suscipit modi, magni
-        voluptate a cupiditate porro eveniet eaque accusamus similique deserunt
-        quas minus? Aperiam aspernatur eligendi vel, consequatur asperiores
-        dolor ab sapiente adipisci! Id dolorem illo quibusdam error veritatis
-        fuga voluptate reprehenderit aliquam omnis magni!
-      </p>
-    </main>
+    </Wrapper>
   );
 };
 export default Landing;
+
+const Wrapper = styled.main`
+  padding-top: 75px;
+  @media screen and (min-width: 950px) {
+    padding-top: 0;
+  }
+`;
