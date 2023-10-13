6c85b04f79ce48a9e2a52fad0aae51d92c8ab5dc
SU-T <guitaristpeet@gmail.com>
2023-10-13 17:12:55 +0700

    initial setup for Testimonial component / install SwiperJS

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -1,9 +1,67 @@
 import styled from "styled-components";
 
+// import Swiper core and modules
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+
+// import Swiper styles
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/navigation";
+
 const Testimonial = ({ bgColor }) => {
   return (
     <Wrapper bgColor={bgColor}>
-      <div>testimonial</div>
+      <Swiper
+        modules={[Autoplay, Navigation]}
+        loop={true}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        slidesPerView={1}
+        speed={1000}
+      >
+        <SwiperSlide>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ea
+              neque accusantium aliquid velit et esse corporis sed, eos
+              voluptate vero, dolores expedita autem iure vel harum,
+              consequuntur quas! Eaque hic inventore possimus quo iste eos, quod
+              facilis atque voluptatem perspiciatis vel, illum quam rerum.
+              Incidunt architecto excepturi hic earum?
+            </p>
+            <h4>- alice bloomberg</h4>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ea
+              neque accusantium aliquid velit et esse corporis sed, eos
+              voluptate vero, dolores expedita autem iure vel harum,
+              consequuntur quas! Eaque hic inventore possimus quo iste eos, quod
+              facilis atque voluptatem perspiciatis vel, illum quam rerum.
+              Incidunt architecto excepturi hic earum?
+            </p>
+            <h4>- alice bloomberg</h4>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ea
+              neque accusantium aliquid velit et esse corporis sed, eos
+              voluptate vero, dolores expedita autem iure vel harum,
+              consequuntur quas! Eaque hic inventore possimus quo iste eos, quod
+              facilis atque voluptatem perspiciatis vel, illum quam rerum.
+              Incidunt architecto excepturi hic earum?
+            </p>
+            <h4>- alice bloomberg</h4>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </Wrapper>
   );
 };
