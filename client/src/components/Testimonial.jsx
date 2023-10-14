2aceb2b5b0202d88ea03626d6fa8397e54d79c7c
SU-T <guitaristpeet@gmail.com>
2023-10-14 17:23:16 +0700

    edited the Attestant file / added navigation to the Testimonial slide / styled Testimonial

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Attestant from "../utils/Attestant";
+import Attestants from "../utils/Attestant";
 
 // import Swiper core and modules
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -13,6 +13,7 @@ import "swiper/css/navigation";
 const Testimonial = ({ bgColor }) => {
   return (
     <Wrapper bgColor={bgColor}>
+      <h1 className="title">what they say about us</h1>
       <Swiper
         modules={[Autoplay, Navigation]}
         loop={true}
@@ -21,47 +22,20 @@ const Testimonial = ({ bgColor }) => {
           disableOnInteraction: false,
         }}
         slidesPerView={1}
-        speed={1000}
+        speed={500}
+        navigation={true}
       >
-        <SwiperSlide>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ea
-              neque accusantium aliquid velit et esse corporis sed, eos
-              voluptate vero, dolores expedita autem iure vel harum,
-              consequuntur quas! Eaque hic inventore possimus quo iste eos, quod
-              facilis atque voluptatem perspiciatis vel, illum quam rerum.
-              Incidunt architecto excepturi hic earum?
-            </p>
-            <h4>- alice bloomberg</h4>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ea
-              neque accusantium aliquid velit et esse corporis sed, eos
-              voluptate vero, dolores expedita autem iure vel harum,
-              consequuntur quas! Eaque hic inventore possimus quo iste eos, quod
-              facilis atque voluptatem perspiciatis vel, illum quam rerum.
-              Incidunt architecto excepturi hic earum?
-            </p>
-            <h4>- alice bloomberg</h4>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae ea
-              neque accusantium aliquid velit et esse corporis sed, eos
-              voluptate vero, dolores expedita autem iure vel harum,
-              consequuntur quas! Eaque hic inventore possimus quo iste eos, quod
-              facilis atque voluptatem perspiciatis vel, illum quam rerum.
-              Incidunt architecto excepturi hic earum?
-            </p>
-            <h4>- alice bloomberg</h4>
-          </div>
-        </SwiperSlide>
+        {Attestants.map((attestant) => {
+          const { name, testimonial } = attestant;
+          return (
+            <SwiperSlide>
+              <div className="slide">
+                <p className="testimonial">{testimonial}</p>
+                <h4 className="attestant">- {name} -</h4>
+              </div>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </Wrapper>
   );
@@ -69,7 +43,62 @@ const Testimonial = ({ bgColor }) => {
 export default Testimonial;
 
 const Wrapper = styled.section`
+  text-align: center;
   background-color: ${(props) => props.bgColor};
   color: var(--white);
   padding: 6em var(--side-contain-sm);
+  .slide {
+    text-align: center;
+  }
+  .testimonial {
+    font-size: 0.85rem;
+    margin: 2em 0;
+    line-height: 1.8;
+    letter-spacing: 2px;
+  }
+  .attestant {
+    font-family: var(--font-2);
+    font-size: 1.1rem;
+    text-transform: capitalize;
+  }
+  .swiper-button-prev,
+  .swiper-button-next {
+    display: none;
+  }
+  @media screen and (min-width: 450px) {
+    .slide {
+      padding: 0 3em;
+    }
+    .swiper-button-prev,
+    .swiper-button-next {
+      display: inline;
+      --swiper-navigation-size: 18px;
+      --swiper-navigation-top-offset: 38%;
+      --swiper-navigation-sides-offset: 0;
+      --swiper-navigation-color: var(--white);
+    }
+  }
+  @media screen and (min-width: 950px) {
+    .title {
+      font-size: 2.5rem;
+    }
+    .slide {
+      max-width: 950px;
+      margin: 0 auto;
+    }
+    .testimonial {
+      margin: 3em 0;
+    }
+    .swiper {
+    }
+  }
+  @media screen and (min-width: 1200px) {
+    .swiper {
+      width: var(--side-contain-max);
+    }
+    .swiper-button-prev,
+    .swiper-button-next {
+      --swiper-navigation-sides-offset: 27px;
+    }
+  }
 `;
--- client/src/utils/Attestant.jsx
+++ client/src/utils/Attestant.jsx
@@ -1,16 +1,18 @@
-const Attestant = [
+const Attestants = [
   {
     name: "alice bloomberg",
-    testimonial: "",
+    testimonial:
+      "How can I begin to describe my time with Poveda... It was simply life-changing! I always imagined that Bali would be a breathtaking destination, but thanks to Poveda, I was able to not just experience the culture, but have once-in-a-lifetime memories. I got so much more than I ever anticipated from my holiday.",
   },
   {
     name: "dan daves",
-    testimonial: "",
+    testimonial:
+      "For a long time I avoided talking about holidays as my girlfriend just went on and on about Bali – I thought it'd be just yoga and beach side meditation, and as an avid water sports person, it didn't sound like up my street. But thanks to Poveda, we were able to get the best of both worlds and have a holiday to remember.",
   },
   {
     name: "emily gillmore",
-    testimonial: "",
+    testimonial: `I'm a foodie, so whenever I go on a trip to a new country, I look for the most authentic, out-there dishes and restaurants to try. Thanks to Poveda, I didn't have to waste my time on Bali looking for the best restaurants on my free time – they took me to the best corners on the island with time to spare for other activities, too.`,
   },
 ];
 
-export default Attestant;
+export default Attestants;
