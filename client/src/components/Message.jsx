e2deb9da0cde369e36f1a9eeea95bdfed8922d7c
SU-T <guitaristpeet@gmail.com>
2023-10-16 21:41:03 +0700

    initial setup for Message component

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,4 +1,15 @@
-const Message = () => {
-  return <div>Message</div>;
+import styled from "styled-components";
+
+const Message = ({ title, paragraph }) => {
+  return (
+    <Wrapper>
+      <div className="container">
+        <h1>{title}</h1>
+        <p>{paragraph}</p>
+      </div>
+    </Wrapper>
+  );
 };
 export default Message;
+
+const Wrapper = styled.section``;
--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,11 +1,16 @@
 import styled from "styled-components";
-import { Hero } from "../components";
+import { Hero, Message } from "../components";
 import imgUrl from "../assets/images/HERO_02_ABOUTUS.jpg";
 
 const AboutUs = () => {
   return (
     <Wrapper>
       <Hero smText={`it's time to`} bgText="know us" imgUrl={imgUrl} />
+
+      <Message
+        title="in love with bali"
+        paragraph={`We are a couple of nomads who used to explore the globe. The day we stepped our foot on Balinese ground one thing was clear - we fell in love. We kept coming back every few months and understood that this is a love affair for a lifetime. We fully moved here in 2018 and been exploring the island ever since. We’re excited to share what we’ve found and show you this beautiful corner of the world.`}
+      />
     </Wrapper>
   );
 };
