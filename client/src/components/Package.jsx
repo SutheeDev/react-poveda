270df7b6b1464ac943a6d3a0ce1dfcddda10e222
SU-T <guitaristpeet@gmail.com>
2023-10-19 21:13:02 +0700

    styled Package component

--- client/src/components/Package.jsx
+++ client/src/components/Package.jsx
@@ -6,14 +6,22 @@ const Package = ({ title, info, price, bgColor, txColor }) => {
     <Wrapper bgColor={bgColor} txColor={txColor}>
       <div className="container">
         <div className="info-container">
-          <h2>{title}</h2>
-          <p>{info}</p>
-          <Button text="learn more" />
+          <h2 className="title">{title}</h2>
+          <p className="info">{info}</p>
+          <Button
+            text="learn more"
+            bgColor="transparent"
+            bdColor={txColor}
+            txColor={txColor}
+            bgColorHover={txColor}
+            bdColorHover={txColor}
+            txColorHover={bgColor}
+          />
         </div>
 
         <div className="price-container">
-          <h2>from</h2>
-          <h1>${price}</h1>
+          <h2 className="from">from</h2>
+          <h1 className="price">${price}</h1>
         </div>
       </div>
     </Wrapper>
@@ -21,4 +29,60 @@ const Package = ({ title, info, price, bgColor, txColor }) => {
 };
 export default Package;
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+  background-color: ${(props) => props.bgColor};
+  color: ${(props) => props.txColor};
+  padding: 6em var(--side-contain-sm);
+
+  .info {
+    font-size: 0.85rem;
+    line-height: 1.8;
+    letter-spacing: 2px;
+  }
+  h2 {
+    margin-bottom: 1em;
+  }
+  .btn-container {
+    margin: 2em 0 5em 0;
+  }
+  .price {
+    font-size: 4rem;
+  }
+  @media screen and (min-width: 950px) {
+    .container {
+      display: flex;
+
+      padding: 0 var(--side-contain-950);
+      max-width: var(--side-contain-max);
+      margin: 0 auto;
+    }
+    .info-container,
+    .price-container {
+      width: 50%;
+    }
+    .price-container {
+      display: flex;
+      justify-content: flex-end;
+      align-items: baseline;
+      align-self: center;
+      gap: 2em;
+    }
+    .from {
+      font-size: 1.7rem;
+    }
+    .price {
+      font-size: 7.5rem;
+    }
+    .btn-container {
+      margin: 4em 0 0 0;
+    }
+    .title {
+      font-size: 2.5rem;
+    }
+  }
+  @media screen and (min-width: 1200px) {
+    .info {
+      font-size: 1rem;
+    }
+  }
+`;
