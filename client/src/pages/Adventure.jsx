b388ffca8883c02d22597b79b9a4301e34f85c79
SU-T <guitaristpeet@gmail.com>
2023-11-04 13:27:12 -0400

    added Message to Adventure page

--- client/src/pages/Adventure.jsx
+++ client/src/pages/Adventure.jsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
-import { Hero } from "../components";
+import {
+  Hero,
+  Message,
+  Article,
+  Package,
+  Invitation,
+  Footer,
+} from "../components";
 import imgUrl from "../assets/images/HERO_04_ADVENTURE.jpg";
 
 const Adventure = () => {
   return (
     <Wrapper>
       <Hero smText={`it's time for`} bgText="action" imgUrl={imgUrl} />
+
+      <Message
+        title="experience heaven"
+        paragraph={`If you want to find as many adventures as you can find, Bali is the place for you. Hop on a scooter, surf the waves or explore the beautiful nature full of exciting wildlife - wherever you go, the island will offer a lot of exciting opportunities for your adventurous heart.`}
+      />
     </Wrapper>
   );
 };
