dc06df755036be6af3f477ffbabbc83bbad6a112
SU-T <guitaristpeet@gmail.com>
2023-11-06 09:22:16 -0500

    initially constructed the sendEmail controller

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,5 @@
 import { StatusCodes } from "http-status-codes";
+import { BadRequestError } from "../errors/index.js";
 import brevoApi from "sib-api-v3-sdk";
 import validator from "validator";
 
@@ -6,5 +7,12 @@ const sendEmail = async (req, res) => {
   const { name, lastname, email, message } = req.body;
 
   if (!name || !email || !message) {
+    throw new BadRequestError("Please fill in all required fields!");
   }
+
+  if (!validator.isEmail(email)) {
+    throw new BadRequestError("Please provide valid email address");
+  }
+
+  res.status(StatusCodes.OK).json({ msg: "Thank you for readching out!" });
 };
